98f0d672f93d4a7bc3d8cc4d26ace8750133df0d
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-09-21 12:27:19 +0400

    Create palindrome.py

---
+++ Small-ideas/palindrome.py
@@ -0,0 +1,12 @@
+#You can find palindrome numbers up #to n and sum of them with the help #of this program:
+
+
+n=int(input("enter the number:"))
+print(f"palindrome numbers from 1 to {n}")
+sum=0
+for i in range(1,n):
+	if str(i)==str(i)[::-1]:
+		sum+=i
+		print(i,end=" ")
+print("\nsum of them:",sum)
+
