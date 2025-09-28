1fec3a16054df9049c9060f47ba8191b2f5cbe59
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-09-28 22:37:19 +0400

    Create problem07_10001st_prime.py

---
+++ ProjectEuler/problem07_10001st_prime.py
@@ -0,0 +1,13 @@
+a = 2
+count = 0
+n=10001
+while count<=n:
+    for i in range(2, a):
+        if a % i == 0:
+            break
+    else:
+        count+=1
+    if count==n:
+                print(f"{count}.prime: {a}")
+                break
+    a+=1
