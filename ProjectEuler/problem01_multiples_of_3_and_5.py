b33e35808f621898ba1c8a8db4da767be52f7164
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-09-28 10:37:41 +0400

    Update problem01_multiples_of_3_and_5.py

--- ProjectEuler/problem01_multiples_of_3_and_5.py
+++ ProjectEuler/problem01_multiples_of_3_and_5.py
@@ -1,20 +1,7 @@
-#1   
-sum=0
-for i in range(0,1000): 
-        if i%3==0 or i%5==0:
-                sum+=i
-print(sum)
-
-#2
 def sumof(n):
-        i=0
-        s=0
-        while i<n:
-                if i%3==0 or i%5==0:
-                        s+=i
-                i+=1
-        return s
-print(sumof(1000))
-
-#3
-print(sum(i for i in range(0,1000) if i%3==0 or i%5==0))
+	s=0
+	for i in range(1,n):
+		if i%3==0 or i%5==0:
+			s+=i
+	return s
+print(f"sum of all the multiples of 3 or 5 below 1000: {sumof(1000)}")
