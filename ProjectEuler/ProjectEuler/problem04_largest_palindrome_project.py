e164a7e82e7081b857e0dbd97a53c2f249b751a9
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-09-28 22:33:35 +0400

    Delete ProjectEuler/ProjectEuler directory

--- ProjectEuler/ProjectEuler/problem04_largest_palindrome_project.py
+++
@@ -1,7 +0,0 @@
-palindrome=[]
-for a in range(100,1000):
-        for b in range(100,1000):
-                c=a*b
-                if str(c)==str(c)[::-1]:
-                        palindrome.append(c)
-print(max(palindrome))
