509699f72e2877a886b5599198c11aa2a31c3d79
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-10-03 09:20:05 +0400

    Update problem06_sum_square_difference.py

--- ProjectEuler/problem06_sum_square_difference.py
+++ ProjectEuler/problem06_sum_square_difference.py
@@ -1,8 +1,8 @@
 #Difference between the square of the sum and the sum of the squares of the first n numbers.
 s=0
 j=0
-m=int(input("enter the number:"))
-for i in range(1,m+1,1):
+n=int(input("enter the number:"))
+for i in range(1,n+1,1):
 	s+=i**2
 	j+=i
 j=j**2
