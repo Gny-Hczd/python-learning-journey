b3c722b1ad6a68384d8fff3024d9c92bfe3c4443
Gunay Hacizade <hacizadegunay19@gmail.com>
2025-11-23 15:43:30 +0400

    Update problem05_smallest_multiple.py

--- ProjectEuler/problem05_smallest_multiple.py
+++ ProjectEuler/problem05_smallest_multiple.py
@@ -1,3 +1,6 @@
-#this code finds the number that #is evenly divisible by all the #numbers from 1 to 20
+#This code finds the number that is evenly divisible by all the numbers from 1 to 20
+
 import math
-print(math.lcm(*range(1,20)))
+print(f"The smallest number evenly divisible by 1 to 20 is: {math.lcm(*range(1,20+1))}")
+
+#The result of it is 232792560
