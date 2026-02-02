7f493b6d73c14e921268be9419b59ebb1e196011
Gunay Hacizade <hacizadegunay19@gmail.com>
2026-02-02 22:10:08 +0400

    Delete problem06_sum_square_difference.py

--- ProjectEuler/problem06_sum_square_difference.py
+++
@@ -1,9 +0,0 @@
-#Difference between the square of the sum and the sum of the squares of the first n numbers.
-sum_of_squares=0
-square_of_sum=0
-n=100
-for i in range(1,n+1,1):
-        sum_of_squares+=i**2
-        square_of_sum+=i
-square_of_sum=square_of_sum**2
-print("difference:",square_of_sum-sum_of_squares)
