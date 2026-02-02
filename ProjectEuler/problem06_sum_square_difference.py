7eb0b780b184475600f310053f9a774d1ca9977f
Gunay Hacizade <hacizadegunay19@gmail.com>
2026-02-02 22:12:33 +0400

    Update problem06_sum_square_difference.py

--- ProjectEuler/problem06_sum_square_difference.py
+++ ProjectEuler/problem06_sum_square_difference.py
@@ -5,3 +5,6 @@ square1=pow(sum([i for i in range(1,101)]),2)
 #difference between the sum of squares of the first 100 natural numbers and the square of the sum of the first 100 natural numbers is
 diff=square1-sum1
 print(diff)
+
+
+#answer is 25164150
