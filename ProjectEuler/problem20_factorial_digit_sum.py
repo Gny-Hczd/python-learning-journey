c157071e426b904b7d1d03d9017814a03db0dda9
Gunay Hacizade <hacizadegunay19@gmail.com>
2026-02-02 23:01:19 +0400

    Update problem20_factorial_digit_sum.py

--- ProjectEuler/problem20_factorial_digit_sum.py
+++ ProjectEuler/problem20_factorial_digit_sum.py
@@ -1,10 +1,12 @@
-number=100
-total=0
-factorial=1
-while number>0:
-	factorial*=number
-	number-=1
-while factorial>0:
-	total+=factorial%10
-	factorial//=10
-print(total)
+#100!
+fac=1
+i=1
+while i<=100:
+	fac*=i
+	i+=1
+sum=0
+#The sum of the digits in the number 100!
+while fac>1:
+	sum=sum+fac%10
+	fac=fac//10
+print(sum)
